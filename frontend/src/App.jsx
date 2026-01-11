c12efdccf871444a435135204bdf82d43be4934e
Santhosh-Kumar-2004 <santhoshkumarv12136@gmail.com>
2026-01-11 13:40:42 +0530

    Added the carema check file jsx into the app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import ARLanding from './pages/ARLanding'
+import CameraCheck from './pages/CameraCheck'
 
 function App() {
 
   return (
     <>
       <ARLanding />
+      <CameraCheck />
     </>
   )
 }
