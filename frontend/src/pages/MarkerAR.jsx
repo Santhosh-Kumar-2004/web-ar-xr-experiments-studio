3ed783d92acd472bb44b074f168b81635ab87862
Santhosh-Kumar-2004 <santhoshkumarv12136@gmail.com>
2026-01-12 13:00:08 +0530

    Updating the importation

--- frontend/src/pages/MarkerAR.jsx
+++ frontend/src/pages/MarkerAR.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import * as THREE from "three";
-import { MindARThree } from "mind-ar/dist/mindar-image-three.prod.js";
+import { MindARThree } from "mind-ar/dist/mindar-image-three.esm.js";
 
 function MarkerAR() {
   useEffect(() => {
