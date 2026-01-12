42a502128b3567e92282ed7e9823b2b293fe215d
Santhosh-Kumar-2004 <santhoshkumarv12136@gmail.com>
2026-01-12 10:36:32 +0530

    UPdated the basic ar jsx importation

--- frontend/src/pages/BasicAR.jsx
+++ frontend/src/pages/BasicAR.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from "react";
 import * as THREE from "three";
-import "./BasicAR.css";
+import "../styles/BasicAR.css";
 
 function BasicAR() {
   const videoRef = useRef(null);
