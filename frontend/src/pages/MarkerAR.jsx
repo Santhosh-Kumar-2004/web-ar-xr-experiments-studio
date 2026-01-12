82a804421b860de4a1193ba9a90850e1f118faa7
Santhosh-Kumar-2004 <santhoshkumarv12136@gmail.com>
2026-01-12 12:59:26 +0530

    Updated thee marker ar js

--- frontend/src/pages/MarkerAR.jsx
+++ frontend/src/pages/MarkerAR.jsx
@@ -1,53 +1,38 @@
 import { useEffect } from "react";
 import * as THREE from "three";
+import { MindARThree } from "mind-ar/dist/mindar-image-three.prod.js";
 
 function MarkerAR() {
   useEffect(() => {
     const startAR = async () => {
-      const MindARThree =
-        window?.MINDAR?.IMAGE?.MindARThree;
-        console.log("window.MINDAR =", window.MINDAR);
-
-      if (!MindARThree) {
-        console.error("MindAR not loaded");
-        return;
-      }
-
       const mindarThree = new MindARThree({
         container: document.body,
-        imageTargetSrc: "/markers/target.mind",
+        imageTargetSrc: "/markers/targets.mind",
       });
 
       const { renderer, scene, camera } = mindarThree;
 
-      // Light
       const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
       scene.add(light);
 
-      // Marker anchor (index 0)
       const anchor = mindarThree.addAnchor(0);
 
-      // Cube
-      const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
-      const material = new THREE.MeshNormalMaterial();
-      const cube = new THREE.Mesh(geometry, material);
+      const cube = new THREE.Mesh(
+        new THREE.BoxGeometry(0.5, 0.5, 0.5),
+        new THREE.MeshNormalMaterial()
+      );
 
       anchor.group.add(cube);
 
       await mindarThree.start();
 
       renderer.setAnimationLoop(() => {
-        cube.rotation.x += 0.01;
         cube.rotation.y += 0.01;
         renderer.render(scene, camera);
       });
     };
 
     startAR();
-
-    return () => {
-      // Optional cleanup later
-    };
   }, []);
 
   return null;
